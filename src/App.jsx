48af639cf7e08103c74d2a55c26c61243042cad1
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-08-14 00:01:33 -0500

    Entendiendo como funciona el useSelector de react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
 
 const App = () => {
+  const taskState = useSelector(state => state.tasks)
+  console.log(taskState)
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>hello World</h1>
+      <TaskForm />
+      <TaskList />
     </div>
   )
 }
